d1b42958427f09be68c7253c6c200d26e5080461
Oleg <amstl@mail.ru>
2024-04-26 17:15:25 +0700

    Create test for model "Product".

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from.models import Category
 
-# Create your tests here.
+
+class TestCategory(TestCase):
+
+    def test_category_model(self):
+        """
+        Test the Category model.
+        """
+        category = Category(name='Test Category', slug='test-category')
+        assert category.name == 'Test Category'
+        assert category.slug == 'test-category'
+        assert str(category) == 'Test Category'
